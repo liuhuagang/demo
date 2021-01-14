34df40bc02f4b0942d4f4f35a0ef260b8442c131
liuhuagang <709777172@qq.com>
2021-01-14 11:30:44 +0800

    test

---
+++ src/main/java/com/example/demo/controller/KeyController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class KeyController {
+
+	// 方式一：String
+
+	@RequestMapping("/")
+	public String list(Map<String, Object> map) {
+		map.put("name", "HelloKitty");
+		map.put("age", 1);
+		System.out.print("11111111");
+		// 返回视图
+		return "list";
+	}
+}
